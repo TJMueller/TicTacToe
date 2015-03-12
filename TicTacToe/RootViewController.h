5996d6f17d3b4b6efc1bd4dc6b10ea5e0c2b0159
TJMueller <timothyjonmueller@gmail.com>
2015-03-12 14:47:50 -0500

    TJ's initial work with labels

--- TicTacToe/RootViewController.h
+++ TicTacToe/RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  TicTacToe
 //
 //  Created by Timothy Mueller on 3/12/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-
---
+++ TicTacToe/RootViewController.m
@@ -0,0 +1,48 @@
+//
+//  RootViewController.m
+//  TicTacToe
+//
+//  Created by Timothy Mueller on 3/12/15.
+//  Copyright (c) 2015 Timothy Mueller. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *LabelOne;
+@property (strong, nonatomic) IBOutlet UILabel *LabelTwo;
+@property (strong, nonatomic) IBOutlet UILabel *LabelThree;
+@property (strong, nonatomic) IBOutlet UILabel *LabelFour;
+@property (strong, nonatomic) IBOutlet UILabel *LabelFive;
+@property (strong, nonatomic) IBOutlet UILabel *LabelSix;
+@property (strong, nonatomic) IBOutlet UILabel *LabelSeven;
+@property (strong, nonatomic) IBOutlet UILabel *LabelEight;
+@property (strong, nonatomic) IBOutlet UILabel *LabelNine;
+@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
