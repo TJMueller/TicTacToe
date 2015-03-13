35feff8e96cae416693e5c3e2bf9ef348022290a
GrumpyDane <micklerche@gmail.com>
2015-03-13 12:23:20 -0500

    ai started but not hooked in yet

--- TicTacToe/RootViewController.m
+++ TicTacToe/RootViewController.m
@@ -30,6 +30,9 @@
 
 @property CGPoint originalCenter;
 @property CGPoint locationOfDrag;
+@property NSArray *combinationArray;
+
+@property NSMutableArray *currentMoves;
 
 
 @end
@@ -41,6 +44,8 @@
     self.whichPlayerLabel.text = @"O";
     self.whichPlayerLabel.textColor = [UIColor redColor];
     self.originalCenter = self.whichPlayerLabel.center;
+    self.combinationArray = [NSArray arrayWithObjects:@"123", @"147", @"159", @"258", @"357", @"369", @"456", @"789", nil];
+    self.currentMoves = [[NSMutableArray alloc] init];
 
 
 }
@@ -173,6 +178,15 @@
 - (void) onAITurn
 {
     if (self.currentPlayer) {
+
+        [self getCurrentMoves];
+
+        int winningMove = [self checkForWinningMove];
+        int blockingMove = [self checkForBlockingMove];
+        int centerMove = [self checkForCenterMove];
+        int nextMove = [self checkForNextMove];
+
+
         for (UILabel *label in self.labels) {
             if ([label.text isEqual: @""]) {
                 label.textColor = (self.currentPlayer? [UIColor blueColor]:[UIColor redColor]);
@@ -187,6 +201,100 @@
         }
     }
 }
+
+
+- (int)checkForCenterMove {
+    return [self.LabelFive.text isEqualToString:@""] ? 5 : 0;
+}
+
+- (int)checkForNextMove {
+    int nextMove = 0;
+    int counter = 1;
+    for (UILabel *label in self.labels) {
+        if ([label.text isEqual: @""]) {
+            nextMove = counter;
+            break;
+        }
+        counter++;
+    }
+    return nextMove;
+}
+
+- (int)checkForWinningMove {
+    return [self checkForMove:@"OO"];
+}
+
+- (int)checkForBlockingMove {
+    return [self checkForMove:@"XX"];
+}
+
+- (int)checkForMove:(NSString *)winBlockString {
+    int winningMove = 0;
+    int winningComboIndex = 0;
+    int counter = 0;
+
+    // search for winning combo
+    for (NSString* currentMove in self.currentMoves) {
+        if ([currentMove isEqual:winBlockString]) {
+            winningComboIndex = counter;
+            break;
+        }
+        counter++;
+    }
+
+    // select winning combo from combo array convert to char array
+    if (winningComboIndex > 0) {
+        NSArray * winningLabelMoves = [self getWinningLabelArray:winningComboIndex];
+
+        // search for empty text, return that move
+        for (NSString *winningLabelMove in winningLabelMoves) {
+            if ([((UILabel *)[self.labels objectAtIndex:[winningLabelMove intValue] - 1]).text isEqualToString:@""]) {
+                winningMove = [winningLabelMove intValue];
+                break;
+            }
+        }
+    }
+
+    return winningMove;
+}
+
+- (NSArray *)getWinningLabelArray:(int)winningComboIndex {
+    // pass in the winning combination of moves index
+    // return an array to foreach through
+    NSString *winningLabelString = self.combinationArray[winningComboIndex];
+    NSArray *winningLabelMoves = [[NSArray alloc] initWithObjects:[winningLabelString substringToIndex:1],
+    [winningLabelString substringWithRange:NSMakeRange(1, 1)],[winningLabelString substringFromIndex:2], nil];
+
+    return winningLabelMoves;
+}
+
+- (void)getCurrentMoves {
+    // loads all current moves into an array
+    // @"123", @"147", @"159", @"258", @"357", @"369", @"456", @"789"    [self.currentMoves removeAllObjects];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelOne.text, self.LabelTwo.text, self.LabelThree.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelOne.text, self.LabelFour.text, self.LabelSeven.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelOne.text, self.LabelFive.text, self.LabelNine.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelTwo.text, self.LabelFive.text, self.LabelEight.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelThree.text, self.LabelFive.text, self.LabelSeven.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelThree.text, self.LabelSix.text, self.LabelNine.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelFour.text, self.LabelFive.text, self.LabelSix.text ]];
+    [self.currentMoves addObject:[NSString stringWithFormat:@"%@%@%@", self.LabelSeven.text, self.LabelEight.text, self.LabelNine.text ]];
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
